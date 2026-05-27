510d544fd8e90c1eff1e2669125bce6df00a168a
zeihu <zeinabhussaintech@gmail.com>
2026-05-27 18:34:58 +0100

    adjusting things for workflow to pass

--- website_scraper.py
+++ website_scraper.py
@@ -1,4 +1,4 @@
 """A script to scrape price and product data from the HTML content on a website."""
 
-import requests
-from bs4 import BeautifulSoup
+# import requests
+# from bs4 import BeautifulSoup
