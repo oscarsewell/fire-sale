3cdf4bbdcb151d0b69999e4c2b35a94af1654f4b
Tomm0Codes <thomasking987@gmail.com>
2026-06-03 16:11:49 +0100

    cleaning docker and changing python to 3.14 on all docker files

---
+++ cleaning/deploy_cleaner.sh
@@ -0,0 +1,16 @@
+# Bash script to build and push Docker image to ECR for the cleaning lambda
+
+set -e  # Exit if any command fails
+
+if [ -z "$account_number" ]; then
+    echo "Error: account_number variable required"
+    exit 1
+fi
+
+aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin ${account_number}.dkr.ecr.eu-west-2.amazonaws.com
+
+docker buildx build -t fire-sale-prod-lambda-cleaning:latest --provenance=false --platform="linux/amd64" .
+
+docker tag fire-sale-prod-lambda-cleaning:latest ${account_number}.dkr.ecr.eu-west-2.amazonaws.com/fire-sale-prod-lambda-cleaning:latest
+
+docker push ${account_number}.dkr.ecr.eu-west-2.amazonaws.com/fire-sale-prod-lambda-cleaning:latest
