c22e080f6f00ea5fbddd2730525aec509e6b1212
Tomm0Codes <thomasking987@gmail.com>
2026-06-04 17:09:22 +0100

    working docker implementation

---
+++ dashboard/deploy_dashboard.sh
@@ -0,0 +1,16 @@
+# Bash script to build and push Docker image to ECR for the dashboard
+
+set -e  # Exit if any command fails
+
+if [ -z "$account_number" ]; then
+    echo "Error: account_number variable required"
+    exit 1
+fi
+
+aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin ${account_number}.dkr.ecr.eu-west-2.amazonaws.com
+
+docker buildx build -t c23-fire-sale-prod-dashboard:latest --provenance=false --platform="linux/amd64" .
+
+docker tag c23-fire-sale-prod-dashboard:latest ${account_number}.dkr.ecr.eu-west-2.amazonaws.com/c23-fire-sale-prod-dashboard:latest
+
+docker push ${account_number}.dkr.ecr.eu-west-2.amazonaws.com/c23-fire-sale-prod-dashboard:latest
