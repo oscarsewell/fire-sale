2ff638fc700e048c7d5fb81b55e8ae2e3a99e971
Oscar <oscar.sewell17@gmail.com>
2026-06-05 10:16:14 +0100

    Add and update deployment scripts to deploy dockerfiles on ECRs

--- cleaning/deploy_cleaner.sh
+++ cleaning/deploy_cleaner.sh
@@ -9,8 +9,8 @@ fi
 
 aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin ${account_number}.dkr.ecr.eu-west-2.amazonaws.com
 
-docker buildx build -t fire-sale-prod-lambda-cleaning:latest --provenance=false --platform="linux/amd64" .
+docker buildx build --no-cache -t c23-fire-sale-prod-lambda-cleaning:latest --provenance=false --platform="linux/amd64" .
 
-docker tag fire-sale-prod-lambda-cleaning:latest ${account_number}.dkr.ecr.eu-west-2.amazonaws.com/fire-sale-prod-lambda-cleaning:latest
+docker tag c23-fire-sale-prod-lambda-cleaning:latest ${account_number}.dkr.ecr.eu-west-2.amazonaws.com/c23-fire-sale-prod-lambda-cleaning:latest
 
-docker push ${account_number}.dkr.ecr.eu-west-2.amazonaws.com/fire-sale-prod-lambda-cleaning:latest
+docker push ${account_number}.dkr.ecr.eu-west-2.amazonaws.com/c23-fire-sale-prod-lambda-cleaning:latest
---
+++ product_checker/deploy_product_checker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Bash script to build and push Docker image to ECR for the product checker lambda
+
+set -e  # Exit if any command fails
+
+if [ -z "$account_number" ]; then
+    echo "Error: account_number variable required"
+    exit 1
+fi
+
+aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin ${account_number}.dkr.ecr.eu-west-2.amazonaws.com
+
+docker buildx build --no-cache -t c23-fire-sale-prod-lambda-tracked-product-checker:latest --provenance=false --platform="linux/amd64" .
+
+docker tag c23-fire-sale-prod-lambda-tracked-product-checker:latest ${account_number}.dkr.ecr.eu-west-2.amazonaws.com/c23-fire-sale-prod-lambda-tracked-product-checker:latest
+
+docker push ${account_number}.dkr.ecr.eu-west-2.amazonaws.com/c23-fire-sale-prod-lambda-tracked-product-checker:latest
