fa170f5e6bb6d9eaa9e47216af0192c67d885b32
Tomm0Codes <thomasking987@gmail.com>
2026-06-10 12:44:54 +0100

    done

--- dashboard/deploy_dashboard.sh
+++ dashboard/deploy_dashboard.sh
@@ -2,6 +2,9 @@
 
 set -e  # Exit if any command fails
 
+# Always build from the project root regardless of where this script is called from
+cd "$(dirname "$0")/.."
+
 if [ -z "$account_number" ]; then
     echo "Error: account_number variable required"
     exit 1
@@ -9,7 +12,7 @@ fi
 
 aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin ${account_number}.dkr.ecr.eu-west-2.amazonaws.com
 
-docker buildx build -f dashboard/Dockerfile -t c23-fire-sale-prod-dashboard:latest --provenance=false --platform="linux/amd64" ..
+docker buildx build -f dashboard/Dockerfile -t c23-fire-sale-prod-dashboard:latest --provenance=false --platform="linux/amd64" .
 
 docker tag c23-fire-sale-prod-dashboard:latest ${account_number}.dkr.ecr.eu-west-2.amazonaws.com/c23-fire-sale-prod-dashboard:latest
 
